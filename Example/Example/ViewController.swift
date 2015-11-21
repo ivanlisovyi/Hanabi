3a34f25a98adf359f8e9df285718fab18a3c9366
Ivan Lisovyi <lisovyi.ivan@gmail.com>
2015-11-21 13:37:24 +0200

    Initial commit

---
+++ Example/Example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Ivan Lisovyi on 21/11/2015.
+//  Copyright © 2015 Ivan Lisovyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
