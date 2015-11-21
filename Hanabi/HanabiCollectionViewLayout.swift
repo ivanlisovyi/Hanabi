24dc54bb4028643fd90570f25985dcc903ab40e7
Ivan Lisovyi <lisovyi.ivan@gmail.com>
2015-11-21 21:44:52 +0200

    added example project

---
+++ Hanabi/HanabiCollectionViewLayout.swift
@@ -0,0 +1,119 @@
+//
+//  HanabiCollectionViewLayout.swift
+//  Hanabi
+//
+//  Created by Ivan Lisovyi on 21/11/2015.
+//  Copyright © 2015 Ivan Lisovyi. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class HanabiCollectionViewLayout: UICollectionViewLayout {
+    @IBInspectable var standartHeight: CGFloat = 100.0
+    @IBInspectable var focusedHeight: CGFloat = 280.0
+    @IBInspectable var dragOffset: CGFloat = 180.0
+    
+    private var cachedLayoutAttributes = [UICollectionViewLayoutAttributes]()
+    
+    // MARK: UICollectionViewLayout methods
+
+    override func collectionViewContentSize() -> CGSize {
+        guard let collectionView = collectionView else {
+            return super.collectionViewContentSize()
+        }
+        
+        let itemsCount = collectionView.numberOfItemsInSection(0)
+        let contentHeight = CGFloat(itemsCount) * dragOffset + (collectionView.frame.height - dragOffset)
+        
+        return CGSize(width: collectionView.frame.width, height: contentHeight)
+    }
+    
+    override func shouldInvalidateLayoutForBoundsChange(newBounds: CGRect) -> Bool {
+        return true
+    }
+    
+    override func targetContentOffsetForProposedContentOffset(proposedContentOffset: CGPoint, withScrollingVelocity velocity: CGPoint) -> CGPoint {
+        let proposedItemIndex = roundf(Float(proposedContentOffset.y / dragOffset))
+        let nearestPageOffset = CGFloat(proposedItemIndex) * dragOffset
+        
+        return CGPoint(x: 0.0, y: nearestPageOffset)
+    }
+    
+    override func layoutAttributesForElementsInRect(rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
+        var layoutAttributes = [UICollectionViewLayoutAttributes]()
+        
+        for attributes in cachedLayoutAttributes {
+            if CGRectIntersectsRect(attributes.frame, rect) {
+                layoutAttributes.append(attributes)
+            }
+        }
+        
+        return layoutAttributes
+    }
+    
+    override func prepareLayout() {
+        guard let collectionView = collectionView else {
+            return
+        }
+        
+        var cache = [UICollectionViewLayoutAttributes]()
+        
+        let itemsCount = collectionView.numberOfItemsInSection(0)
+        var frame = CGRectZero
+        var y: CGFloat = 0.0
+        
+        for item in 0..<itemsCount {
+            let indexPath = NSIndexPath(forItem: item, inSection: 0)
+            
+            var height = standartHeight
+            let currentFocusedIndex = currentFocusedItemIndex()
+            let nextItemOffset = nextItemPercentageOffset(forFocusedItemIndex: currentFocusedIndex)
+            
+            if indexPath.item == currentFocusedIndex {
+                y = collectionView.contentOffset.y - standartHeight * nextItemOffset
+                height = focusedHeight
+            } else if indexPath.item == (currentFocusedIndex + 1) && indexPath.item != itemsCount {
+                height = standartHeight + max((focusedHeight - standartHeight) * nextItemOffset, 0)
+                
+                let maxYOffset = y + standartHeight
+                y = maxYOffset - height
+            } else {
+                y = frame.origin.y + frame.height
+            }
+            
+            frame = CGRect(x: 0, y: y, width: collectionView.frame.width, height: height)
+            
+            let attributes = UICollectionViewLayoutAttributes(forCellWithIndexPath: indexPath)
+            attributes.zIndex = item
+            attributes.frame = frame
+            
+            cache.append(attributes)
+            
+            y = CGRectGetMaxY(frame)
+        }
+        
+        cachedLayoutAttributes = cache
+    }
+    
+    override func layoutAttributesForItemAtIndexPath(indexPath: NSIndexPath) -> UICollectionViewLayoutAttributes? {
+        return cachedLayoutAttributes[indexPath.item]
+    }
+    
+    // MARK: Private methods
+    
+    private func currentFocusedItemIndex() -> Int {
+        guard let collectionView = collectionView else {
+            return 0
+        }
+        
+        return max(0, Int(collectionView.contentOffset.y / dragOffset))
+    }
+    
+    private func nextItemPercentageOffset(forFocusedItemIndex index: Int) -> CGFloat {
+        guard let collectionView = collectionView else {
+            return 0;
+        }
+        
+        return (collectionView.contentOffset.y / dragOffset) - CGFloat(index)
+    }
+}
---
+++ HanabiExample/HanabiExample/ContentViewController.swift
@@ -0,0 +1,72 @@
+//
+//  ContentViewController.swift
+//  Example
+//
+//  Created by Ivan Lisovyi on 21/11/2015.
+//  Copyright © 2015 Ivan Lisovyi. All rights reserved.
+//
+
+import UIKit
+
+class ContentViewController: UIViewController {
+    @IBOutlet weak var collectionView: UICollectionView?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupNavigationBar()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return .LightContent
+    }
+
+    private func setupNavigationBar() {
+        let label = UILabel(frame: CGRectMake(0, 0, 200, 21))
+        label.font = UIFont.systemFontOfSize(21)
+        label.textColor = UIColor.darkGrayColor()
+        label.textAlignment = .Center
+        label.text = "Genres"
+        
+        navigationItem.titleView = label
+    }
+}
+
+extension ContentViewController: UICollectionViewDataSource {
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cellIdentifier = "cellIdentifier"
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(cellIdentifier, forIndexPath: indexPath) as! ContentCollectionViewCell
+        cell.imageView?.image = imageAtIndex(indexPath.row)
+        
+        return cell
+    }
+    
+    func imageAtIndex(index: Int) -> UIImage? {
+        switch index {
+        case 0:
+            return UIImage(named: "harem")
+        case 1:
+            return UIImage(named: "school")
+        case 2:
+            return UIImage(named: "mecha")
+        default: break
+        }
+        
+        return nil
+    }
+}
+
+extension ContentViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAtIndex section: Int) -> CGFloat {
+        return 0
+    }
+}
