d802d08f905808f4493db4bea0a38998ff7aeebb
Ivan Lisovyi <lisovyi.ivan@gmail.com>
2016-03-10 13:25:58 +0200

    some changes to the project structure

---
+++ HanabiCollectionViewLayout/HanabiCollectionViewLayout/HanabiCollectionViewLayout.h
@@ -0,0 +1,17 @@
+//
+//  HanabiCollectionViewLayout.h
+//  HanabiCollectionViewLayout
+//
+//  Created by Ivan Lisovyi on 10/03/2016.
+//  Copyright © 2016 Ivan Lisovyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HanabiCollectionViewLayout.
+FOUNDATION_EXPORT double HanabiCollectionViewLayoutVersionNumber;
+
+//! Project version string for HanabiCollectionViewLayout.
+FOUNDATION_EXPORT const unsigned char HanabiCollectionViewLayoutVersionString[];
+
+
